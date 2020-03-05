672e5b6e0dc8f3ad163235992d94ae1ee4e06268
Essy Mwangi <sonnieessy@gmail.com>
2020-03-05 09:23:25 +0300

    added a galary on description and styled it

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,47 @@ const navSlide =() => {
 }
 
 navSlide();
+
+function openModal(){
+  document.getElementById('my-modal').style.display ="inline-flex";
+}
+
+function closeModal(){
+  document.getElementById('my-modal').style.display ="none";
+
+}
+var slideIndexJS = 1;
+
+showSlides(slideIndexJS);
+
+function changeSlides(n){
+  showSlides(slideIndexJS += n);
+  
+}
+
+function currentslide(n){
+  showSlides(slideIndexJS = n)
+}
+
+function showSlides(n){
+  var i;
+
+  var slidesJS = document.getElementsByClassName("my-slides");
+
+  if(n > slidesJS.length) {slideIndexJS = 1;}
+
+  if(n < 1) {slideIndexJS = slidesJS.length;}
+
+
+  for (i=0; i < slidesJS.length; i++){
+    slidesJS[i].style.display = "none";
+  }
+
+  slidesJS[slideIndexJS-1].style.display = "block";
+
+
+
+
+}
+
+
